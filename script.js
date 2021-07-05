447bdb9b18dea8c8e05f97c91028f98776a521c5
Nicholas Perez <nicholas.perez290@gmail.com>
2021-07-05 19:45:09 -0400

    adding links to projects, linkedin and resume

---
+++ script.js
@@ -0,0 +1,14 @@
+var project1 = document.querySelector("#project-1");
+var project2 = document.querySelector("#project-2");
+var project3 = document.querySelector("#project-3");
+var project4 = document.querySelector("#project-4");
+var project5 = document.querySelector("#project-5");
+project1.addEventListener("click", function(){
+    document.location = "https://ahuffma2.github.io/comics-assembled/";
+})
+project2.addEventListener("click", function(){
+    document.location = "https://nicholasperez290.github.io/coding-quiz-1/";
+})
+project3.addEventListener("click", function(){
+    document.location = "https://nicholasperez290.github.io/work-day-scheduler/";
+})
